88602e5b2dfce4660c906ae035e3424cea2dfc31
HowardJoness <143377834+HowardJoness@users.noreply.github.com>
2025-05-26 21:50:16 +0800

    画了一个任务栏背景的控件
    （但是我不知道为什么它卡住了

--- task_show.gd
+++ task_show.gd
@@ -9,8 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if GameManager.task != "":
+		$NinePatchRect.visible = false
 		$Task.text = "任务：" + GameManager.task
 	else:
+		$NinePatchRect.visible = false
 		$Task.text = ""
 	
 	$Tips.text = GameManager.tips
